147a105e6c6d8bf2843e86bbbddcc2d5ef47c045
Gagan Saroya <gssaroya@uwaterloo.ca>
2016-03-26 15:14:03 -0400

    bash

--- terriblehack.sh
+++ terriblehack.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-$wav = $1
-$myzip = ""
-if [$# -ne 1];
-    then $myzip = $2
-fi
+wav=$1
+myzip=$2
+name=$3
+
+echo $wav
+echo $myzip
 
 # Extract midi file
-$midi = zipinfo -1 myzip.zip
-curl -sS $myzip > myzip.zip
-unzip myzip.zip
-rm myzip.zip
+y | wget $myzip -O myzip.zip
+midi=`zipinfo -1 myzip.zip`
+echo $midi
+y | unzip myzip.zip
+y | rm myzip.zip
 
 # Convert midi to mp3
-timidity -Ow -o - $midi | lame - $wav.mp3
+y | timidity -Ow -o - $midi | lame - ${name}midi.mp3
+y | ffmpeg -i ${name}midi.mp3 -af "volume=0.35" ${name}low.mp3
+
+# Convery wav to mp3
+y | lame $wav ${name}wav.mp3
+
+# Merge together
+y | ffmpeg -i ${name}low.mp3 -i ${name}wav.mp3 -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 ${name}.mp3
